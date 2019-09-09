09039dbcb1df8518a4a89107d0ad72c7a5848422
7138gmhampton <0407435@abertay.ac.uk>
2019-09-09 12:30:22 +0100

    Added main navbar

--- CMP306CWork/index.php
+++ CMP306CWork/index.php
@@ -16,6 +16,30 @@
 
     </head>
     <body>
+        <!--Main Navbar-->
+        <?php
+        include 'parts/mainbar.html';
+        ?>
+        <!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">CMP306</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainPages">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="mainPages">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="wk1Dropdown" role="button"
+                           data-toggle="dropdown">Week 1</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Practical</a>
+                            <a class="dropdown-item" href="#">Comments</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>-->
+
         <footer>
             <p>Gareth M. Hampton</p>
             <p>0407435</p>
