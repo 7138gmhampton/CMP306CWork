fce980eaf88ec3495457c8e04654243923c2a69c
7138gmhampton <0407435@abertay.ac.uk>
2019-09-09 16:14:44 +0100

    Added week 1 practical page

---
+++ CMP306CWork/scripts/server/week1practical.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>CMP306 Coursework Week 1 Practical</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include 'parts/mainbar.html';
+    ?>
+
+    <footer>
+        <p>Gareth M. Hampton</p>
+        <p>0407435</p>
+    </footer>
+</body>
+</html>
