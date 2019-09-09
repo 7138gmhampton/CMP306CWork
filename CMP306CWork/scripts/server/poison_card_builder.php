cb81d8344c85b13b5fe470dfce20880d35cd15e2
7138gmhampton <0407435@abertay.ac.uk>
2019-09-09 15:51:30 +0100

    Added function to build cards with summaries of each poison.

---
+++ CMP306CWork/scripts/server/poison_card_builder.php
@@ -0,0 +1,13 @@
+<?php
+function buildPoisonCard($name, $alt_name, $image, $description)
+{
+    echo '<div class="card">';
+    echo '  <img class="card-img-left" src="' . $image . '" />';
+    echo '  <div class="card-body">';
+    echo '      <h5 class="card-title">' . $name . '</h5>';
+    echo '      <h6 class="card-subtitle text-muted">' . $alt_name . '</h6>';
+    echo '      <p class="card-text">' . $description . '</p>';
+    echo '  </div>';
+    echo '</div>';
+}
+?>
