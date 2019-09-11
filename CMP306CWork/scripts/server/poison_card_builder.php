99eaecd2dfe835ef69222b61a8918204a20f8765
7138gmhampton <0407435@abertay.ac.uk>
2019-09-11 15:12:47 +0100

    Changed image class

--- CMP306CWork/scripts/server/poison_card_builder.php
+++ CMP306CWork/scripts/server/poison_card_builder.php
@@ -2,7 +2,7 @@
 function buildPoisonCard($name, $alt_name, $image, $description)
 {
     echo '<div class="card">';
-    echo '  <img class="card-img-left" src="images/' . $image . '" />';
+    echo '  <img class="card-img-top" src="' . $image . '" />';
     echo '  <div class="card-body">';
     echo '      <h5 class="card-title">' . $name . '</h5>';
     echo '      <h6 class="card-subtitle text-muted">' . $alt_name . '</h6>';
