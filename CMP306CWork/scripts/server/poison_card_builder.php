dc603b14498469e196a6bb1463b8c939bd15b2e3
7138gmhampton <0407435@abertay.ac.uk>
2019-09-11 14:33:05 +0100

    Made text in card bodies small

--- CMP306CWork/scripts/server/poison_card_builder.php
+++ CMP306CWork/scripts/server/poison_card_builder.php
@@ -6,7 +6,7 @@ function buildPoisonCard($name, $alt_name, $image, $description)
     echo '  <div class="card-body">';
     echo '      <h5 class="card-title">' . $name . '</h5>';
     echo '      <h6 class="card-subtitle text-muted">' . $alt_name . '</h6>';
-    echo '      <p class="card-text">' . $description . '</p>';
+    echo '      <p class="card-text"><small>' . $description . '</small></p>';
     echo '  </div>';
     echo '</div>';
 }
