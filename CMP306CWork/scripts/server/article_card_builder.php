e8ca540179c6e3e706668a8c658a229405dfbb89
7138gmhampton <0407435@abertay.ac.uk>
2019-09-12 17:51:43 +0100

    Created card builder for article groups

---
+++ CMP306CWork/scripts/server/article_card_builder.php
@@ -0,0 +1,12 @@
+<?php
+function buildArticleCard($name,$image,$image_title,$description)
+{
+    echo '<div class="card">';
+    echo ' <img class="card-img-top" src"'.$image.'" title="'.$image_title.'" />';
+    echo '  <div class="card-body">';
+    echo '      <h5 class="card-title">' . $name . '</h5>';
+    echo '      <p class="card-text">' . $description . '</p>';
+    echo '  </div>';
+    echo '</div>';
+}
+?>
