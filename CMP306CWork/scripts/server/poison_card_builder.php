a30b0d0df9b325fdc85c4970d8d2b7608d5854a4
7138gmhampton <0407435@abertay.ac.uk>
2019-09-16 12:38:28 +0100

    Created new card builder for poison summaries

--- CMP306CWork/scripts/server/poison_card_builder.php
+++ CMP306CWork/scripts/server/poison_card_builder.php
@@ -10,4 +10,20 @@ function buildPoisonCard($name, $alt_name, $image, $description)
     echo '  </div>';
     echo '</div>';
 }
+
+function buildSummaryCard($name, $alt_name, $description, $pic_source, $pic_title, $pic_alt_text)
+{
+    echo '<div class="card">';
+    echo '  <img class="card-img-top" src="' . $image . '" title="'.$pic_title.'" alt="'.
+        $pic_alt_text.'" />';
+    echo '  <div class="card-body">';
+    echo '      <h5 class="card-title">' . $name . '</h5>';
+    echo '      <h6 class="card-subtitle text-muted">' . $alt_name . '</h6>';
+    echo '      <p class="card-text">' . $description . '</p>';
+    echo '  </div>';
+    echo '  <div class="card-body justify-content-center">';
+    echo '      <a class="btn btn-primary" href="#">Go to Articles</a>';
+    echo '  </div>';
+    echo '</div>';
+}
 ?>
