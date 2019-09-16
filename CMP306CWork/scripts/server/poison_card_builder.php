99219e9faedbcf1a5428bc00d4ff102c23109a36
7138gmhampton <0407435@abertay.ac.uk>
2019-09-16 14:57:35 +0100

    Corrected variable name

--- CMP306CWork/scripts/server/poison_card_builder.php
+++ CMP306CWork/scripts/server/poison_card_builder.php
@@ -14,7 +14,7 @@ function buildPoisonCard($name, $alt_name, $image, $description)
 function buildSummaryCard($name, $alt_name, $description, $pic_source, $pic_title, $pic_alt_text)
 {
     echo '<div class="card">';
-    echo '  <img class="card-img-top" src="' . $image . '" title="'.$pic_title.'" alt="'.
+    echo '  <img class="card-img-top" src="' . $pic_source . '" title="'.$pic_title.'" alt="'.
         $pic_alt_text.'" />';
     echo '  <div class="card-body">';
     echo '      <h5 class="card-title">' . $name . '</h5>';
