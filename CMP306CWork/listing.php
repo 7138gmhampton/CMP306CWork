81b64c055275192df0ebcf110a64fe36eb72f4bc
7138gmhampton <0407435@abertay.ac.uk>
2019-09-16 14:55:02 +0100

    Added article listing page

---
+++ CMP306CWork/listing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>Article Listing</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include 'parts/mainbar.html';
+    ?>
+
+    <!--Title Display-->
+
+    <!--Listing of Articles-->
+</body>
+</html>
