07a6918bb96712cc4e44ccf126e50932abaef28b
7138gmhampton <0407435@abertay.ac.uk>
2019-09-16 16:04:02 +0100

    Added poison object getters

--- CMP306CWork/scripts/server/poison.php
+++ CMP306CWork/scripts/server/poison.php
@@ -8,5 +8,14 @@ class Poison
     private $source;
     private $title;
     private $alttext;
+
+    //Access Functions
+    public function getID() { return $this->poison; }
+    public function getName() { return $this->name; }
+    public function getAlternative() { return $this->alternative; }
+    public function getDescription() { return $this->description; }
+    public function getMainPic() { return $this->source; }
+    public function getPicTitle() { return $this->title; }
+    public function getPicAltText() { return $this->alttext; }
 }
 ?>
