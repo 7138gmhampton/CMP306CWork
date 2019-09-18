1f981d43c062a3dd4cc5c02abac1d974638e8d29
7138gmhampton <0407435@abertay.ac.uk>
2019-09-18 17:20:24 +0100

    Added page to display articles

---
+++ CMP306CWork/article.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>Article</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!--Main Navbar-->    
+    <?php
+    include 'parts/mainbar.html';
+    ?>
+
+    <!--Title and Author-->
+    <header>
+
+    </header>
+    <!--Video (If available)-->
+
+    <!--Text of Article-->
+
+    <!--<div class="container">
+        <footer>
+            <p>Gareth M. Hampton</p>
+            <p>0407435</p>
+        </footer>
+    </div>-->    <?php
+    include 'parts/footer.html';
+    ?>
+</body>
+</html>
