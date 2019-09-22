0a55de7773e44624a2ef20a987e1e90cd96801bb
7138gmhampton <0407435@abertay.ac.uk>
2019-09-22 22:10:20 +0100

    Add card builder function to poison class

--- CMP306CWork/scripts/server/poison.php
+++ CMP306CWork/scripts/server/poison.php
@@ -17,5 +17,23 @@ class Poison
     public function getMainPic() { return $this->source; }
     public function getPicTitle() { return $this->title; }
     public function getPicAltText() { return $this->alttext; }
+
+    //Other Functions
+    public function buildCard()
+    {
+        echo '<div class="card">';
+        echo '  <img class="card-img-top" src="' . $this->source . '" title="'.
+            $this->title.'" alt="'.$this->alttext.'" />';
+        echo '  <div class="card-body">';
+        echo '      <h5 class="card-title">' . $this->name . '</h5>';
+        echo '      <h6 class="card-subtitle text-muted">' . $this->alternative . '</h6>';
+        echo '      <p class="card-text">' . $this->description . '</p>';
+        echo '  </div>';
+        echo '  <div class="card-body justify-content-center">';
+        echo '      <a class="btn btn-primary" href="listing.php?id='.$this->poison.
+            '">Go to Articles</a>';
+        echo '  </div>';
+        echo '</div>';
+    }
 }
 ?>
