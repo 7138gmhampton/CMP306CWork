fe74814b8d7d85609d615d810160f1bcff1d20fc
7138gmhampton <0407435@abertay.ac.uk>
2019-09-23 16:09:52 +0100

    Add article and article API classes per MVC framework

---
+++ CMP306CWork/scripts/server/view/article.php
@@ -0,0 +1,46 @@
+<?php
+class Article
+{
+    private $article;
+    private $title;
+    private $author;
+    private $video;
+    private $text;
+
+    //Access Functions
+    public function getID() { return $this->article; }
+    public function getTitle() { return $this->title; }
+    public function getAuthor() { return $this->author; }
+    public function getVideo() { return $this->video; }
+    public function getText() { return $this->text; }
+
+    //Other Functions
+    public function getSummary()
+    {
+        $no_of_chars = strlen($this->text);
+        $summary = null;
+
+        if ($no_of_chars > 200) {
+            $summary = substr($this->text, 0, 197);
+            $summary = $summary . '...';
+        }
+        else $summary = $this->text;
+
+        return $summary;
+    }
+
+    public function buildCard()
+    {
+        echo '<div class="card">';
+        echo '  <div class="card-header">';
+        echo '      <a class="card-title h5" href ="display_article.php?id='.$this->article.'">'.
+            $this->title.'</a>';
+        echo '      <h6 class="card-subtitle">'.$this->author.'</h6>';
+        echo '  </div>';
+        echo '  <div class="card-body">';
+        echo '      <p class="card-text">'.$this->getSummary().'</p>';
+        echo '  </div>';
+        echo '</div>';
+    }
+}
+?>
