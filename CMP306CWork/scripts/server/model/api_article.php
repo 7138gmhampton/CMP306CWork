3e7c91af9544dcd606beebeb22b0d9a217e0d66b
7138gmhampton <0407435@abertay.ac.uk>
2019-09-25 17:54:49 +0100

    Add function to get all articles for a given poison

--- CMP306CWork/scripts/server/model/api_article.php
+++ CMP306CWork/scripts/server/model/api_article.php
@@ -3,6 +3,31 @@ require_once 'database.php';
 
 class ArticleAPI extends Database
 {
-    
+    public static function getAllArticlesForPoison($poison_id)
+    {
+        $command = 'SELECT psn_poison_article.article, psn_article.title, psn_article.author, '.
+            'psn_article.video, psn_article.text '.
+            'FROM psn_poison_article '.
+            'JOIN psn_article ON psn_poison_article.article = psn_article.article '.
+            'WHERE psn_poison_article.poison = :poison_id';
+        $result = null;
+
+        try {
+            $connection = self::makeConnection();
+            $statement = $connection->prepare($command);
+            $statement->bindParam(':poison_id', $poison_id);
+
+            $statement->execute();
+
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e) {
+            echo 'Error: '.$e->getMessage();
+            return null;
+        }
+
+        $response = json_encode($result);
+        return $response;
+    }
 }
 ?>
