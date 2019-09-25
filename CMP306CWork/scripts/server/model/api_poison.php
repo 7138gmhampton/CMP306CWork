79a59e88790afe0a13e068464ec1cc2fbb025457
7138gmhampton <0407435@abertay.ac.uk>
2019-09-25 17:24:03 +0100

    Add function to get a poison by its ID to API

--- CMP306CWork/scripts/server/model/api_poison.php
+++ CMP306CWork/scripts/server/model/api_poison.php
@@ -28,7 +28,36 @@ class PoisonAPI extends Database
         //var_dump($result);
         $response = json_encode($result);
         //var_dump($response);
+        return $response;
+    }
 
+    public static function getPoisonById($id)
+    {
+        $command = 'SELECT psn_poison.poison, psn_poison.name, psn_poison.alternative, '.
+            'psn_poison.description, psn_image.source, psn_image.title, psn_image.alttext '.
+            'FROM psn_poison '.
+            'INNER JOIN psn_image '.
+            'ON psn_poison.main_pic = psn_image.image '.
+            'WHERE poison = :id';
+        $result = null;
+
+        if (self::rowCount('psn_poison', 'poison', $id) == 1 ) {
+            try {
+                $connection = self::makeConnection();
+                $statement = $connection->prepare($command);
+                $statement->bindParam(':id', $id);
+
+                $statement->execute();
+
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            }
+            catch (PDOException $e) {
+                echo 'Error: '.$e->getMessage();
+                return null;
+            }
+        }
+
+        $response = json_encode($result);
         return $response;
     }
 }
