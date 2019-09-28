1cee0db7ee4b0ec63e41a1dcad7674e3b16b4afc
7138gmhampton <0407435@abertay.ac.uk>
2019-09-28 16:11:18 +0100

    Add image API class and function to get a single article image

---
+++ CMP306CWork/scripts/server/model/api_image.php
@@ -0,0 +1,33 @@
+<?php
+require_once ROOT.'scripts/server/model/database.php';
+
+class ImageAPI extends Database
+{
+    public function getFirstArticleImage($article_id)
+    {
+        $command = 'SELECT psn_image.image, psn_image.source, psn_image.title, psn_image.alttext '.
+            'FROM psn_article_image '.
+            'JOIN psn_image ON psn_article_image.image = psn_image.image '.
+            'WHERE article = :id '.
+            'LIMIT 1';
+        $result = null;
+
+        try {
+            $connection = self::makeConnection();
+            $statement = $connection->prepare($command);
+            $statement->bindParam(':id', $article_id);
+
+            $statement->execute();
+
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e) {
+            echo 'Error: '.$e->getMessage();
+            return null;
+        }
+
+        $response = json_encode($result);
+        return $response;
+    }
+}
+?>
