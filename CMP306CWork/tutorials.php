762f19ab69e46d428b3597f132c0933123a2e43c
7138gmhampton <0407435@abertay.ac.uk>
2019-09-28 19:17:20 +0100

    Switch out dodgy characters

--- CMP306CWork/tutorials.php
+++ CMP306CWork/tutorials.php
@@ -16,6 +16,8 @@ include_once 'config.php';
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <script src="scripts/client/tab_handler.js"></script>
 </head>
 <body>
     <!--Main Navbar-->
@@ -45,11 +47,11 @@ include_once 'config.php';
                 <p>
                     Bootstrap employs a flexible grid: 12 columns within each row and these components
             will shrink or grow with the viewport. Elements can be set to take up differing
-            numbers of columns – e.g. a 4-column element will try to fill a third of the row
-            container’s width. More complicated arrangements can be achieved by nesting rows,
+            numbers of columns - e.g. a 4-column element will try to fill a third of the row
+            container's width. More complicated arrangements can be achieved by nesting rows,
             and their attendant columns, within other columns. To prevent smaller/narrower
             screens becoming overwhelmed, particular columnar arrangements can be set for
-            certain ‘classes’ of screen width. For example, a row with 4 columns in a row for
+            certain 'classes' of screen width. For example, a row with 4 columns in a row for
             screens classed as large or bigger can be set to rearrange the items in 2 columns
             for medium screens and a single column for anything smaller.
                 </p>
@@ -58,11 +60,11 @@ include_once 'config.php';
                 <h2 class="h2">Tutorial Question 2</h2>
                 <p>
                     Bootstrap has classes that can make deployment of dropdown menus, pagination and
-            navs simple. The ‘broadest’ menu selections will be included in the navbar which
+            navs simple. The 'broadest' menu selections will be included in the navbar which
             will appear at the top of every page. More specific usages will be applied utilising
             the Bootstrap classes as necessary. To ensure accurate replication of menus (in
             particular the main navbar), the menus will be written in separate PHP/HTML files
-            and then ‘included’ on the required pages using PHP scripting.
+            and then 'included' on the required pages using PHP scripting.
                 </p>
             </div>
             <div class="container mt-3 border">
@@ -175,7 +177,7 @@ include_once 'config.php';
                                 <p>
                                     This collapsible group allows for only a single section to be
                             expanded at a time. This ensures that the group cannot be entirely
-                            expanded – taking up unnecessary space. N.B. This text is contained
+                            expanded - taking up unnecessary space. N.B. This text is contained
                             within an example of such a grouping.
                                 </p>
                             </div>
@@ -193,5 +195,7 @@ include_once 'config.php';
     <?php
     include_once ROOT.'parts/footer.html';
     ?>
+
+    <!--<script src="scripts/client/common.js"></script>-->
 </body>
 </html>
