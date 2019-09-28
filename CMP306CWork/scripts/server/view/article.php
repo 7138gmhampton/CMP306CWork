d122b45cdb14fd2773cf05b591a5467eacd41c0c
7138gmhampton <0407435@abertay.ac.uk>
2019-09-28 16:25:11 +0100

    Begin cardcuilder with pic and add optional parameters to constructor

--- CMP306CWork/scripts/server/view/article.php
+++ CMP306CWork/scripts/server/view/article.php
@@ -6,14 +6,17 @@ class Article
     private $author;
     private $video;
     private $text;
+    private $image;
 
-    function __construct($article, $title, $author, $video, $text)
+    function __construct($article, $title, $author, $video, $text, $image=null, $source=null,
+        $img_title=null,$alttext=null)
     {
         $this->article = $article;
         $this->title = $title;
         $this->author = $author;
         $this->video = $video;
         $this->text = $text;
+        
     }
 
     //Access Functions
@@ -50,6 +53,26 @@ class Article
         echo '      <p class="card-text">'.$this->getSummary().'</p>';
         echo '  </div>';
         echo '</div>';
+    }
+
+    public function buildCardWithPic()
+    {
+        echo '<div class="card">';
+        echo '  <div class="row no-gutters">';
+        echo '      <div class="col-lg-4">';
+        echo '          <img src="'
+        echo '      </div>';
+        echo '      <div class="col-lg-8">';
+        echo '          <div class="card-header">';
+        echo '              <a class="card-title h5" href ="display_article.php?id='.
+                                $this->article.'">'.$this->title.'</a>';
+        echo '              <h6 class="card-subtitle">'.$this->author.'</h6>';
+        echo '          </div>';
+        echo '          <div class="card-body">';
+        echo '              <p class="card-text">'.$this->getSummary().'</p>';
+        echo '          </div>';
+        echo '      </div>';
+        echo '  </div>';
     }
 }
 ?>
