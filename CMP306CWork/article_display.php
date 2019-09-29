6bae91f71a8bafe3d6e2f6f869e3ef7421e300e7
7138gmhampton <0407435@abertay.ac.uk>
2019-09-29 12:56:26 +0100

    Add article display page that conforms to MVC architecture

---
+++ CMP306CWork/article_display.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>Article</title> <!--TODO: Dynamic title-->
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include_once ROOT.'parts/mainbar.html';
+    ?>
+
+    <article>
+        <!--Title and Author-->
+        <header>
+            <?php
+
+            ?>
+        </header>
+
+        <!--Carousel of Images-->
+
+        <!--Text of Article-->
+
+        <!--More About Video-->
+
+    </article>
+    
+    <!--Footer-->
+    <?php
+    include_once ROOT.'parts/footer.html';
+    ?>
+</body>
+</html>
