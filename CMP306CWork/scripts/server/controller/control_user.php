4cc188968c906b2993853ab273ca3f5627f2c5bd
7138gmhampton <0407435@abertay.ac.uk>
2019-09-30 11:21:55 +0100

    Add function to create a new user

--- CMP306CWork/scripts/server/controller/control_user.php
+++ CMP306CWork/scripts/server/controller/control_user.php
@@ -1,6 +1,9 @@
 <?php
 class UserController
 {
-    
+    public static function create($email, $username, $raw_password)
+    {
+        
+    }
 }
 ?>
--- CMP306CWork/scripts/server/model/api_user.php
+++ CMP306CWork/scripts/server/model/api_user.php
@@ -1,8 +1,31 @@
 <?php
 require_once ROOT.'scripts/server/model/database.php';
+require_once ROOT.'scripts/server/view/user.php';
 
 class UserAPI extends Database
 {
-    
+    public static function createUser($user_data)
+    {
+        $commmand = 'INSERT INTO psn_user (email, username, password) '.
+            'VALUES (:email, :username, :password)';
+        $user_array = json_decode($user_data, true);
+        $user = new User(
+            $user_array[0]['email'],
+            $user_array[0]['username'],
+            $user_array[0]['password']);
+
+        try {
+            $statement = self::prepareStatement($commmand);
+            $statement->bindParam(':email', $user->getEmail());
+            $statement->bindParam(':username', $user->getUsername());
+            $statement->bindParam(':password', $user->getPassword());
+
+            $statement->execute();
+        }
+        catch (PDOException $e) {
+            echo 'Error: '.$e->getMessage();
+            return null;
+        }
+    }
 }
 ?>
