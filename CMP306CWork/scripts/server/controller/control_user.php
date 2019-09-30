154211923c9cd99dc5749746d051717a8a9ab459
7138gmhampton <0407435@abertay.ac.uk>
2019-09-30 12:32:44 +0100

    Add function to create new user

--- CMP306CWork/scripts/server/controller/control_user.php
+++ CMP306CWork/scripts/server/controller/control_user.php
@@ -1,9 +1,15 @@
 <?php
+require_once ROOT.'scripts/server/model/api_user.php';
+
 class UserController
 {
     public static function create($email, $username, $raw_password)
     {
-        
+        $password = password_hash($raw_password, PASSWORD_DEFAULT);
+        $user_array = array('email' => $email, 'username' => $username, 'password' => $password);
+        $user_data = json_encode($user_array);
+
+        UserAPI::createUser($user_data);
     }
 }
 ?>
