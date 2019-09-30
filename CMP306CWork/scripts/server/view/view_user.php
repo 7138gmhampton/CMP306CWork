977019853932cd0a309a2c43888e0196cc169e60
7138gmhampton <0407435@abertay.ac.uk>
2019-09-30 15:48:20 +0100

    Add function to get a user from their email

--- CMP306CWork/scripts/server/view/view_user.php
+++ CMP306CWork/scripts/server/view/view_user.php
@@ -1,5 +1,6 @@
 <?php
 require_once ROOT.'scripts/server/model/api_user.php';
+require_once ROOT.'scripts/server/view/user.php';
 
 class UserView
 {
@@ -11,6 +12,20 @@ class UserView
         $valid = count($user_array) == 1;
 
         return $valid;
+    }
+
+    public static function singleByEmail($email)
+    {
+        $user_data = UserAPI::getUserByEmail($email);
+        $user_array = json_decode($user_data, true);
+
+        $user = new User(
+            $user_array[0]['email'],
+            $user_array[0]['username'],
+            $user_array[0]['password'],
+            $user_array[0]['user']);
+
+        return $user;
     }
 }
 ?>
