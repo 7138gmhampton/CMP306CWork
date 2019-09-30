00d74c95abd9992738f9fef295d6b4acf3f69c70
7138gmhampton <0407435@abertay.ac.uk>
2019-09-30 12:17:04 +0100

    Create user view and add function to verify a user

---
+++ CMP306CWork/scripts/server/view/view_user.php
@@ -0,0 +1,16 @@
+<?php
+require_once ROOT.'scripts/server/model/api_user.php';
+
+class UserView
+{
+    public static function valid($email)
+    {
+        $user_data = UserAPI::valid($email);
+        $user_array = json_decode($user_data, true);
+
+        $valid = count($user_array) == 1;
+
+        return $valid;
+    }
+}
+?>
