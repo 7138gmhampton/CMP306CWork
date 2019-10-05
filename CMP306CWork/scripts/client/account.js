5fb647e32ab8993c969df0ed066692d568af86ff
7138gmhampton <0407435@abertay.ac.uk>
2019-10-05 16:21:03 +0100

    Correct regex - put in brackets

--- CMP306CWork/scripts/client/account.js
+++ CMP306CWork/scripts/client/account.js
@@ -12,13 +12,31 @@
 
     $('#btnRegister').click(function ()
     {
-        let password = document.getElementById('inputRegisterPassword').textContent;
-        let confirmation = document.getElementById('inputRegisterConfirm').textContent;
+        let password = document.getElementById('inputRegisterPassword').value;
+        let confirmation = document.getElementById('inputRegisterConfirm').value;
+
+        let special = /[!"#\$%&'\(\)\*\+,-\.:;<=>\?@\[\\\]\^_`\{\|\}~]/;
+        let number = /\w/;
+        let capital = /[A-Z]/;
+        let letter = /[a-z]/;
+
+        console.log('Password: ' + password);
+        console.log('Confirm: ' + confirmation);
+
+        console.log('Special: ' + special.test(password));
+        console.log('Number: ' + password.test(number));
+        console.log('Capital: ' + password.test(capital));
+        console.log('Letter: ' + password.test(letter));
 
         if (password !== confirmation)
             document.getElementById('txtRegisterPasswordError').innerText = 'Password and ' +
                 'confirmation do not match!';
-        else document.getElementById('formRegister').submit();
+        else if (!(password.test(special) && password.test(number) && password.test(capital) && 
+            password.test(letter) || password.length() > 14))
+            document.getElementById('txtRegisterPasswordError').innerText = 'Password MUST ' +
+                'contain at least one lowercase, Uppercase, number and special character ' +
+                '(#, !, ...) OR be at least 15 characters long.';
+        //else document.getElementById('formRegister').submit();
     });
 
     $('#inputRegisterUsername').change(function ()
