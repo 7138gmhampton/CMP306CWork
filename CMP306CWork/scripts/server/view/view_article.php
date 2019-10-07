62cc15cb128e46ebb3ed81dd219eec7ebdb47946
7138gmhampton <0407435@abertay.ac.uk>
2019-10-07 15:17:43 +0100

    Switch to sanitised arrays

--- CMP306CWork/scripts/server/view/view_article.php
+++ CMP306CWork/scripts/server/view/view_article.php
@@ -10,11 +10,13 @@ class ArticleView extends ViewController
     {
         $article_data = ArticleAPI::getAllArticlesForPoison(self::scrub($poison_id));
         $article_array = json_decode($article_data, true);
-        $all_articles = array();
+        $scrubbed_articles = self::scrubArray($article_array);
 
-        foreach ($article_array as $each_article) {
+        $all_articles = array();
+        foreach ($scrubbed_articles as $each_article) {
             $image_data = ImageAPI::getMainImageForArticle($each_article['article']);
             $image_array = json_decode($image_data, true);
+            $scrubbed_images = self::scrubArray($image_array);
             if (!$image_array) {
                 $next_article = new Article(
                     $each_article['article'],
@@ -31,10 +33,10 @@ class ArticleView extends ViewController
                     $each_article['author'],
                     $each_article['video'],
                     $each_article['text'],
-                    $image_array[0]['image'],
-                    $image_array[0]['source'],
-                    $image_array[0]['title'],
-                    $image_array[0]['alttext']);
+                    $scrubbed_images[0]['image'],
+                    $scrubbed_images[0]['source'],
+                    $scrubbed_images[0]['title'],
+                    $scrubbed_images[0]['alttext']);
                 array_push($all_articles, $next_article);
             }
         }
@@ -47,13 +49,14 @@ class ArticleView extends ViewController
         //$article_data = ArticleAPI::getArticleById($article_id);
         $article_array = json_decode(ArticleAPI::getArticleById(
             self::scrub($article_id)), true);
+        $scrubbed_array = self::scrubArray($article_array);
 
         $article = new Article(
-            $article_array[0]['article'], 
-            $article_array[0]['title'],
-            $article_array[0]['author'],
-            $article_array[0]['video'],
-            $article_array[0]['text']);
+            $scrubbed_array[0]['article'],
+            $scrubbed_array[0]['title'],
+            $scrubbed_array[0]['author'],
+            $scrubbed_array[0]['video'],
+            $scrubbed_array[0]['text']);
 
         return $article;
     }
