fd16bcd83730e54854a98f77f368f8634e34552f
7138gmhampton <0407435@abertay.ac.uk>
2019-10-07 14:35:57 +0100

    Switch to scrubbed array

--- CMP306CWork/scripts/server/view/view_user.php
+++ CMP306CWork/scripts/server/view/view_user.php
@@ -19,12 +19,14 @@ class UserView extends ViewController
     {
         $user_data = UserAPI::getUserByEmail(self::scrub($email));
         $user_array = json_decode($user_data, true);
+        $scrubbed_array = self::scrubArray($user_array);
+        //var_dump($scrubbed_array);
 
         $user = new User(
-            $user_array[0]['email'],
-            $user_array[0]['username'],
-            $user_array[0]['password'],
-            $user_array[0]['user']);
+            $scrubbed_array[0]['email'],
+            $scrubbed_array[0]['username'],
+            $scrubbed_array[0]['password'],
+            $scrubbed_array[0]['user']);
 
         return $user;
     }
