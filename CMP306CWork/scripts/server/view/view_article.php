fa16bf752c02825c79c7539093ba057da86399a8
7138gmhampton <0407435@abertay.ac.uk>
2019-10-07 15:18:50 +0100

    Revert "Switch to sanitised arrays"
    This reverts commit 88d3bde95e82b597557f33f21b94e5dd85d74264.

--- CMP306CWork/scripts/server/view/view_article.php
+++ CMP306CWork/scripts/server/view/view_article.php
@@ -10,13 +10,11 @@ class ArticleView extends ViewController
     {
         $article_data = ArticleAPI::getAllArticlesForPoison(self::scrub($poison_id));
         $article_array = json_decode($article_data, true);
-        $scrubbed_articles = self::scrubArray($article_array);
-
         $all_articles = array();
-        foreach ($scrubbed_articles as $each_article) {
+
+        foreach ($article_array as $each_article) {
             $image_data = ImageAPI::getMainImageForArticle($each_article['article']);
             $image_array = json_decode($image_data, true);
-            $scrubbed_images = self::scrubArray($image_array);
             if (!$image_array) {
                 $next_article = new Article(
                     $each_article['article'],
@@ -33,10 +31,10 @@ class ArticleView extends ViewController
                     $each_article['author'],
                     $each_article['video'],
                     $each_article['text'],
-                    $scrubbed_images[0]['image'],
-                    $scrubbed_images[0]['source'],
-                    $scrubbed_images[0]['title'],
-                    $scrubbed_images[0]['alttext']);
+                    $image_array[0]['image'],
+                    $image_array[0]['source'],
+                    $image_array[0]['title'],
+                    $image_array[0]['alttext']);
                 array_push($all_articles, $next_article);
             }
         }
@@ -49,14 +47,13 @@ class ArticleView extends ViewController
         //$article_data = ArticleAPI::getArticleById($article_id);
         $article_array = json_decode(ArticleAPI::getArticleById(
             self::scrub($article_id)), true);
-        $scrubbed_array = self::scrubArray($article_array);
 
         $article = new Article(
-            $scrubbed_array[0]['article'],
-            $scrubbed_array[0]['title'],
-            $scrubbed_array[0]['author'],
-            $scrubbed_array[0]['video'],
-            $scrubbed_array[0]['text']);
+            $article_array[0]['article'], 
+            $article_array[0]['title'],
+            $article_array[0]['author'],
+            $article_array[0]['video'],
+            $article_array[0]['text']);
 
         return $article;
     }
