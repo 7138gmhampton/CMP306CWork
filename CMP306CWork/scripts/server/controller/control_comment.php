bf7ba4bbcb52ee0b4e1ba0279bcb709ee0bbb683
7138gmhampton <0407435@abertay.ac.uk>
2019-10-14 15:50:49 +0100

    Add creation function to controller

--- CMP306CWork/scripts/server/controller/control_comment.php
+++ CMP306CWork/scripts/server/controller/control_comment.php
@@ -1,8 +1,18 @@
 <?php
+require_once ROOT.'scripts/server/view_controller.php';
 require_once ROOT.'scripts/server/model/api_comment.php';
 
-class CommentController
+class CommentController extends ViewController
 {
-    
+    public static function create($article_id, $user_id, $content)
+    {
+        $comment_array = array(
+            'article' => self::scrub($article_id),
+            'user' => self::scrub($user_id),
+            'content' => self::scrub($content));
+        $comment_data = json_encode($comment_array);
+
+        CommentAPI::createComment($comment_data);
+    }
 }
 ?>
