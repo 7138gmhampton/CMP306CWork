fb523cb9cec00fb845e215114efcf07a632f17a9
7138gmhampton <0407435@abertay.ac.uk>
2019-10-14 17:58:05 +0100

    Start function to begin the comment editing process

--- CMP306CWork/scripts/client/article.js
+++ CMP306CWork/scripts/client/article.js
@@ -3,5 +3,13 @@
     $('#btnComment').click(function ()
     {
         document.getElementById('formComment').submit();
-    })
-})
+    });
+
+    $('button[id^="btnEdit"]').click(function (event)
+    {
+        let button_id = event.target.id;
+        //console.log('Comment ' + button_id);
+        let user_id = button_id.substring(7);
+        //console.log('Comment ' + user_id);
+    });
+});
