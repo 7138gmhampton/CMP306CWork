f8ff078b968c0f271b4ecfdc7c9fa0dcd25b2414
7138gmhampton <0407435@abertay.ac.uk>
2019-10-14 14:46:03 +0100

    Add function to get all comments for an article from the database

--- CMP306CWork/scripts/server/model/api_comment.php
+++ CMP306CWork/scripts/server/model/api_comment.php
@@ -3,6 +3,17 @@ require_once ROOT.'scripts/server/model/database.php';
 
 class CommentAPI extends Database
 {
-    
+    public static function getAllCommentsForArticle($article_id)
+    {
+        $command = 'SELECT psn_comment.comment, psn_comment.content, psn_comment.user, '.
+            'psn_user.username '.
+            'FROM psn_comment '.
+            'JOIN psn_user ON psn_comment.user = psn_user.user '.
+            'WHERE psn_comment.article = :article_id';
+
+        $response = self::selectWhere($command, ':article_id', $article_id);
+
+        return $response;
+    }
 }
 ?>
