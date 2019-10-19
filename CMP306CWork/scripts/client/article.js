e077fb6e5a547c666d2c2057b9fb16b5ed998f73
7138gmhampton <0407435@abertay.ac.uk>
2019-10-19 18:11:55 +0100

    Ajax retrieval of comment

--- CMP306CWork/scripts/client/article.js
+++ CMP306CWork/scripts/client/article.js
@@ -11,13 +11,24 @@
         //console.log('Comment ' + button_id);
         let user_id = button_id.substring(7);
         //console.log('Comment ' + user_id);
-        $('#modalEdit').modal('show');
+
+        document.getElementById('inputEditId').value = user_id;
+        $.post('scripts/server/view/show_comment.php', { comment: user_id }, function (data)
+        {
+            document.getElementById('inputEditOriginal').value = data.content;
+            //console.log('Flag');
+            //console.log(data.content);
+            document.getElementById('inputEditReplace').value = data.content;
+            $('#modalEdit').modal('show');
+        }, 'json');
+
+        //$('#modalEdit').modal('show');
     });
 
     $('#btnEditSubmit').click(function ()
     {
-        let original = document.getElementById('inputOriginal').value;
-        let replacement = document.getElementById('inputReplace').value;
+        let original = document.getElementById('inputEditOriginal').value;
+        let replacement = document.getElementById('inputEditReplace').value;
 
         if (replacement === original) $('#btnEditCancel').click();
         else document.getElementById('formEdit').submit();
