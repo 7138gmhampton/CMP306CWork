679cad8175396ef1021bf0dbe0182d8aa81f59fe
7138gmhampton <0407435@abertay.ac.uk>
2019-10-19 17:48:08 +0100

    Switch to get superglobal

--- CMP306CWork/scripts/server/view/show_comment.php
+++ CMP306CWork/scripts/server/view/show_comment.php
@@ -2,7 +2,7 @@
 require_once '../../../config.php';
 require_once ROOT.'scripts/server/view/view_comment.php';
 
-$comment = CommentView::single($_POST['comment']);
+$comment = CommentView::single($_GET['comment']);
 
 $comment_array = array('comment' => $comment->getID(), 'content' => $comment->getContent());
 $comment_send = json_encode($comment_array);
