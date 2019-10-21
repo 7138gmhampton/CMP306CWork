b9eaf4bb947fb6d798cfd71eb340b96d1eee452a
7138gmhampton <0407435@abertay.ac.uk>
2019-10-21 12:02:36 +0100

    White space

--- CMP306CWork/scripts/server/controller/edit_comment.php
+++ CMP306CWork/scripts/server/controller/edit_comment.php
@@ -2,4 +2,5 @@
 require_once '../../../config.php';
 require_once ROOT.'scripts/server/controller/control_comment.php';
 
-CommentController::edit($_POST['comment'], $_POST['content']);?>
+CommentController::edit($_POST['comment'], $_POST['content']);
+?>
