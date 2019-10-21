4ae9a30146401f9b878d042e114a929cf52d3f04
7138gmhampton <0407435@abertay.ac.uk>
2019-10-21 19:07:44 +0100

    Create common function library and add cookie getter function

---
+++ CMP306CWork/scripts/client/common.js
@@ -0,0 +1,16 @@
+﻿function getCookie(key)
+{
+    key += '=';
+    let decoded_cookie = decodeURIComponent(document.cookie);
+    let cookie_array = decoded_cookie.split(';');
+
+    for (let iii = 0; iii < cookie_array.length; ++iii) {
+        let each_entry = cookie_array[iii];
+        while (each_entry.charAt(0) == ' ') each_entry = each_entry.substring(1);
+
+        if (each_entry.indexOf(key) == 0)
+            return each_entry.substring(key.length, each_entry.length);
+    }
+
+    return '';
+}
