79115e20dcf6883c313de1054678614ae42b37d9
7138gmhampton <0407435@abertay.ac.uk>
2019-11-11 13:05:51 +0000

    Create practical ten page

---
+++ CMP306CWork/practical_ten.php
@@ -0,0 +1,41 @@
+<?php
+//Session
+session_start();
+include_once 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>CMP306 Coursework Week 10</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include_once ROOT.'parts/mainbar.php';
+    ?>
+
+    <!--RSS Button-->
+    <div class="container">
+        <a class="btn btn-primary" href="https://mayar.abertay.ac.uk/~0407435/CMP306cwk/scripts/server/view/article_feed.php">
+            Last Five Articles
+        </a>
+    </div>
+
+    <!--Footer-->
+    <?php
+    include_once ROOT.'parts/footer.html';
+    ?>
+
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
