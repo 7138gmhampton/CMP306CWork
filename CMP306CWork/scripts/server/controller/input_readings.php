eb64ed09b8138e5e76a3b0f5bd2c9d522cc6e874
7138gmhampton <0407435@abertay.ac.uk>
2019-12-06 14:40:44 +0000

    Implement script to receive and upload sensor readings

--- CMP306CWork/scripts/server/controller/input_readings.php
+++ CMP306CWork/scripts/server/controller/input_readings.php
@@ -1,3 +1,9 @@
 <?php
+require_once '../../../config.php';
+require_once ROOT.'scripts/server/controller/control_iot.php';
 
+$device_id = $_POST['device'];
+$sensor_readings = $_POST['sensors'];
+
+IoTController::create($device_id, $sensor_readings);
 ?>
