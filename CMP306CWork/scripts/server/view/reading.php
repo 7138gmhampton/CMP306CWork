05bdc3b4112b9090e7ec32f244787b1252e1b9e6
7138gmhampton <0407435@abertay.ac.uk>
2019-12-06 16:19:54 +0000

    Create reading class

---
+++ CMP306CWork/scripts/server/view/reading.php
@@ -0,0 +1,26 @@
+<?php
+class Reading
+{
+    private $device_id;
+    private $voltage;
+    private $internal_temp;
+    private $external_temp;
+    private $light_level;
+
+    function __construct($device_id, $voltage, $int_temp, $ext_temp, $light_lvl)
+    {
+        $this->device_id = $device_id;
+        $this->voltage = $voltage;
+        $this->internal_temp = $int_temp;
+        $this->external_temp = $ext_temp;
+        $this->light_level = $light_lvl;
+    }
+
+    //Access Functions
+    public function getId() { return $this->device_id; }
+    public function getVoltage() { return $this->voltage; }
+    public function getIntTemp() { return $this->internal_temp; }
+    public function getExtTemp() { return $this->external_temp; }
+    public function getLight() { return $this->light_level; }
+}
+?>
