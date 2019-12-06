fab3d6e5202574627b9e995121c986d60269351a
7138gmhampton <0407435@abertay.ac.uk>
2019-12-06 15:45:41 +0000

    Addat input script to utilise json in

--- CMP306CWork/scripts/server/controller/input_readings.php
+++ CMP306CWork/scripts/server/controller/input_readings.php
@@ -1,9 +1,15 @@
 <?php
+//Error Reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../../../config.php';
 require_once ROOT.'scripts/server/controller/control_iot.php';
 
-$device_id = $_POST['device'];
-$sensor_readings = $_POST['sensors'];
+//$device_id = $_POST['this_device'];
+//$sensor_readings = $_POST['sensors'];
+$http_body = file_get_contents('php://input');
+$post = json_decode($http_body);
 
-IoTController::create($device_id, $sensor_readings);
+IoTController::create($post->this_device, $post->sensors);
 ?>
