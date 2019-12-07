0b0afb20b84697dbd2506dd880b0d9f1d7ad432b
7138gmhampton <0407435@abertay.ac.uk>
2019-12-07 16:44:10 +0000

    Create script to ajax request the current reading

---
+++ CMP306CWork/scripts/client/mobile_always_current.js
@@ -0,0 +1,28 @@
+﻿function update()
+{
+    $.get("scripts/server/view/get_current_reading.php", function (data)
+    {
+        //alert(data.device);
+        $('#textDevice').text(data.device);
+        $('#textVoltage').text(data.voltage + 'V');
+        $('#textInternal').text(data.internal + '\xB0C Internal');
+        $('#textExternal').text(data.external + '\xB0C External');
+        $('#textLight').text(data.light);
+    }, 'json');
+    //alert('Checked');
+};
+
+$(document).ready(function ()
+{
+    //$.get("scripts/server/view/get_current_reading.php", function (data)
+    //{
+    //    //alert(data.device);
+    //    $('#textDevice').text(data.device);
+    //    $('#textVoltage').text(data.voltage + 'V');
+    //    $('#textInternal').text(data.internal + '\xB0C Internal');
+    //    $('#textExternal').text(data.external + '\xB0C External');
+    //    $('#textLight').text(data.light);
+    //}, 'json');
+    update();
+    setInterval(update, 3000);
+});
