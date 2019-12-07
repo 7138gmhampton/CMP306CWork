83e1b7aa5596617b472b841e76713f308fea647f
7138gmhampton <0407435@abertay.ac.uk>
2019-12-07 14:06:51 +0000

    Switch to absolute addresses for jqm assets

--- CMP306CWork/mobile_ten_latest.php
+++ CMP306CWork/mobile_ten_latest.php
@@ -10,17 +10,17 @@ require_once 'config.php';
 <head>
     <meta charset="utf-8" />
     <meta name="author" content="Gareth M. Hampton" />
-    <base href="https://demos.jquerymobile.com/1.4.5/pages-single-page/" />
+    <!--<base href="https://demos.jquerymobile.com/1.4.5/pages-single-page/" />-->
     <meta name="viewport" content="width = device-width, initial-scale = 1" />
 
     <title>CMP306 Week 9 Most Recent</title>
 
-    <link rel="stylesheet" href="../css/themes/default/jquery.mobile-1.4.5.min.css" />
-    <link rel="stylesheet" href="../_assets/css/jqm-demos.css" />
-    <link rel="shortcut icon" href="../favicon.ico" />
-    <script src="../js/jquery.js"></script>
-    <script src="../_assets/js/index.js"></script>
-    <script src="../js/jquery.mobile-1.4.5.min.js"></script>
+    <link rel="stylesheet" href="https://demos.jquerymobile.com/1.4.5/css/themes/default/jquery.mobile-1.4.5.min.css" />
+    <link rel="stylesheet" href="https://demos.jquerymobile.com/1.4.5/_assets/css/jqm-demos.css" />
+    <link rel="shortcut icon" href="https://demos.jquerymobile.com/1.4.5/favicon.ico" />
+    <script src="https://demos.jquerymobile.com/1.4.5/js/jquery.js"></script>
+    <script src="https://demos.jquerymobile.com/1.4.5/_assets/js/index.js"></script>
+    <script src="https://demos.jquerymobile.com/1.4.5/js/jquery.mobile-1.4.5.min.js"></script>
     
 </head>
 <body class="ui-mobile-viewport ui-overlay-a">
@@ -54,5 +54,8 @@ require_once 'config.php';
             </table>
         </div>
     </div>
+
+    <!--Other Scripts-->
+    <script src="scripts/client/mobile_ten_latest.js"></script>
 </body>
 </html>
