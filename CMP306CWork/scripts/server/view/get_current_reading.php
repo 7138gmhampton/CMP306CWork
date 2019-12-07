f161292ae15a506cea0b1721fcc9c37bafb12209
7138gmhampton <0407435@abertay.ac.uk>
2019-12-07 16:41:52 +0000

    Convert reading object to assoc array for json encode

--- CMP306CWork/scripts/server/view/get_current_reading.php
+++ CMP306CWork/scripts/server/view/get_current_reading.php
@@ -1,9 +1,20 @@
 <?php
+//Error Reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../../../config.php';
 require_once ROOT.'scripts/server/view/view_iot.php';
 
 $current = IoTView::current();
+$current_array = array(
+    'timestamp' => $current->getTime(),
+    'device' => $current->getId(),
+    'voltage' => $current->getVoltage(),
+    'internal' => $current->getIntTemp(),
+    'external' => $current->getExtTemp(),
+    'light' => $current->getLight());
 
-$response = json_encode($current);
+$response = json_encode($current_array);
 echo $response;
 ?>
