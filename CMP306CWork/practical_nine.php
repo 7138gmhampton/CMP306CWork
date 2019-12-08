e41f57e45cb6be7f4dd4dd00bb1b39ed1e98883e
7138gmhampton <0407435@abertay.ac.uk>
2019-12-08 15:57:07 +0000

    Create week nine practical page that acts as a portal to the mobile section

---
+++ CMP306CWork/practical_nine.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include_once 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>CMP306 Week Nine Practical</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include_once ROOT.'parts/mainbar.php';
+    ?>
+
+    <!--Mobile Section-->
+    <div class="container text-center">
+        <a class="btn btn-primary" href="mobile_current.php">Go to Mobile Section</a>
+    </div>
+
+    <!--Footer-->
+    <?php
+    include_once ROOT.'parts/footer.html';
+    ?>
+
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
