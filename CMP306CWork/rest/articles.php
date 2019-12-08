952e323b44b3d2988a0d58c38aa60c10979f6ac9
7138gmhampton <0407435@abertay.ac.uk>
2019-12-08 17:33:03 +0000

    Add restful get and switch case

--- CMP306CWork/rest/articles.php
+++ CMP306CWork/rest/articles.php
@@ -1,3 +1,24 @@
 <?php
+require_once '../config.php';
+require_once ROOT.'scripts/server/view/view_article.php';
 
+function get_article($id)
+{
+    $article = ArticleView::single($id);
+
+    $article_array = array(
+        'article' => $article->getID(),
+        'title' => $article->getTitle(),
+        'author' => $article->getAuthor(),
+        'video'=> $article->getVideo(),
+        'text'=>$article->getText(),
+        'image'=>$article->getImage());
+
+    $response = json_encode($article_array);
+    echo $response;
+}
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET': get_article($_GET['id']); break;
+}
 ?>
