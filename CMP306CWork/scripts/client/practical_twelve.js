c17812d78cb7e92b154ce855a2c584ffd1abb64a
7138gmhampton <0407435@abertay.ac.uk>
2019-12-08 19:27:42 +0000

    Add function to populate the list of articles

---
+++ CMP306CWork/scripts/client/practical_twelve.js
@@ -0,0 +1,25 @@
+﻿function populateList()
+{
+    $.get('rest/articles.php', function (data)
+    {
+        let list = document.getElementById('listArticles');
+
+        for (let iii = 0; iii < data.length; ++iii) {
+            let item = document.createElement('li');
+            let button = document.createElement('button');
+            let title = document.createTextNode(data[iii].title);
+
+            button.setAttribute('value', data[iii].article);
+            button.setAttribute('class', 'list-group-item list-group-item-action');
+
+            button.appendChild(title);
+            item.appendChild(button);
+            list.appendChild(item);
+        }
+    }, 'json');
+}
+
+$(document).ready(function ()
+{
+    populateList();
+});
