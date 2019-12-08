5cf0a69488ce916a465b4f20f983c4e4db5bf6f4
7138gmhampton <0407435@abertay.ac.uk>
2019-12-08 18:41:29 +0000

    Create page sekeleton for week 12

---
+++ CMP306CWork/practical_twelve.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gareth M. Hampton" />
+
+    <title>CMP306 Week 12 Practical</title>
+
+    <link rel="stylesheet" type="text/css" href="general_styles.css" />
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body>
+    <!--Main Navbar-->
+    <?php
+    include_once ROOT.'parts/mainbar.php';
+    ?>
+
+    <!--Footer-->
+    <?php
+    include_once ROOT.'parts/footer.html';
+    ?>
+
+    <!--Bs Js-->
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
